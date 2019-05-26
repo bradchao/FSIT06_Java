e38cbf61d90eb662d1ceabc0b1fbc393b594606d
admin <admin@MBP>
2019-05-26 11:09:07 +0800

    201905261109

--- Brad2Java/src/tw/brad/myjava/Brad99.java
+++ Brad2Java/src/tw/brad/myjava/Brad99.java
@@ -3,13 +3,22 @@ package tw.brad.myjava;
 public class Brad99 {
 
 	public static void main(String[] args) {
-		for (int j=1; j<=9; j++) {
-			for (int i=2 ; i<=5 ; i++) {
-				int r = i * j;
-				System.out.printf("%d x %d = %d\t", i, j, r);
+		int row = 3;
+		int col = 3;
+		int start = 1;
+		
+		for (int k=0; k<row; k++) {
+			for (int j=1; j<=9; j++) {
+				for (int i=start ; i< start + col ; i++) {
+					int newi = i + k * col;
+					int r = newi * j;
+					System.out.printf("%d x %d = %d\t", newi, j, r);
+				}
+				System.out.println();
 			}
-			System.out.println();
+			System.out.println("----");
 		}
+		
 		
 	}
 
