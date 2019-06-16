eb87f9354a8f7839c4f2bd21d4ee4680993ca635
admin <admin@MBP>
2019-06-16 14:32:09 +0800

    201906161432

--- Brad2Java/src/tw/brad/apps/Brad38.java
+++ Brad2Java/src/tw/brad/apps/Brad38.java
@@ -2,21 +2,32 @@ package tw.brad.apps;
 
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 public class Brad38 {
 
 	public static void main(String[] args) {
 		byte[] buf = new byte[1024];
-		try {
-			DatagramSocket socket = new DatagramSocket(8888);
-			DatagramPacket packet = new DatagramPacket(buf, buf.length);
-			System.out.println("before");
-			socket.receive(packet);
-			System.out.println("after");
-			socket.close();
-			System.out.println("receive ok");
-		}catch(Exception e) {
-			System.out.println(e.toString());
+		while (true) {
+			try {
+				DatagramSocket socket = new DatagramSocket(8888);
+				DatagramPacket packet = new DatagramPacket(buf, buf.length);
+				socket.receive(packet);
+				socket.close();
+				
+				InetAddress urip = packet.getAddress();
+				int len = packet.getLength();
+				byte[] data = packet.getData();
+				String mesg = new String(data,0,len);
+				System.out.println(urip + ":" + mesg);
+
+				if (mesg.contentEquals("end")) {
+					break;
+				}
+				
+			}catch(Exception e) {
+				System.out.println(e.toString());
+			}
 		}
 	}
 
