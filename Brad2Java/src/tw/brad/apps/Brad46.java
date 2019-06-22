dac2e5c765b904bbb043e457a8f6361c4dca78d9
admin <admin@10.0.105.82>
2019-06-22 16:04:52 +0800

    201906221604

--- Brad2Java/src/tw/brad/apps/Brad46.java
+++ Brad2Java/src/tw/brad/apps/Brad46.java
@@ -4,6 +4,10 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.Properties;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -37,15 +41,36 @@ public class Brad46 {
 	
 	static void parseJSON(String json) {
 		try {
+			Properties prop = new Properties();
+			prop.put("user", "root");
+			prop.put("password", "root");
+			prop.put("serverTimezone", "Asia/Taipei");
+			Connection conn = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3307/cust", 
+					prop);
+			PreparedStatement pstmt = conn.prepareStatement(
+					"INSERT INTO gift (gid,pname,feature,url) VALUES (?,?,?,?)");			
+			
 			JSONArray root = new JSONArray(json);
 			for (int i=0; i<root.length(); i++) {
 				JSONObject row = root.getJSONObject(i);
 
+				String gid = row.getString("ID");
 				String pname = row.getString("Name");
-				System.out.println(pname);
-			
+				String feature = row.getString("Feature");
+				String url = row.getString("Column1");
+				
+				pstmt.setString(1, gid);
+				pstmt.setString(2, pname);
+				pstmt.setString(3, feature);
+				pstmt.setString(4, url);
+				pstmt.executeUpdate();
 			
 			}
+			
+			
+			System.out.println("OK");
+			
 		}catch(Exception e) {
 			System.out.println(e.toString());
 		}
