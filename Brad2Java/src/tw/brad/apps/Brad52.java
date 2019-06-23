9664c3f400f268478c553f0a9a02526d9eeac018
admin <admin@MBP.r2101.tciii.org.tw>
2019-06-23 17:05:37 +0800

    201906231705

--- Brad2Java/src/tw/brad/apps/Brad52.java
+++ Brad2Java/src/tw/brad/apps/Brad52.java
@@ -3,10 +3,13 @@ package tw.brad.apps;
 public class Brad52 {
 
 	public static void main(String[] args) {
-		Thread1 t1 = new Thread1();
-		Thread1 t2 = new Thread1();
+		Thread1 t1 = new Thread1("A");
+		Thread1 t2 = new Thread1("B");
+		Brad521 b1 = new Brad521("C");
+		Thread t3 = new Thread(b1);
 		t1.start();
 		t2.start();
+		t3.start();
 		System.out.println("OK");
 		
 	}
@@ -14,11 +17,29 @@ public class Brad52 {
 }
 
 class Thread1 extends Thread {
+	String name;
+	Thread1(String name){this.name = name;}
 	@Override
 	public void run() {
 		super.run();
 		for (int i=0; i<10; i++) {
-			System.out.println(i);
+			System.out.println(name + ":" +i);
+			try {
+				Thread.sleep(100);
+			}catch(InterruptedException ie) {
+				break;
+			}
+		}
+	}
+}
+
+class Brad521 implements Runnable {
+	String name;
+	Brad521(String name){this.name = name;}
+	@Override
+	public void run() {
+		for (int i=0; i<10; i++) {
+			System.out.println(name + ":" +i);
 			try {
 				Thread.sleep(100);
 			}catch(InterruptedException ie) {
@@ -27,3 +48,4 @@ class Thread1 extends Thread {
 		}
 	}
 }
+
