fb467a0948761d6835be9c403bb8e82254d3337a
admin <admin@MBP.r2101.tciii.org.tw>
2019-06-29 15:09:41 +0800

    201906291509

--- Brad2Java/src/tw/brad/apps/Brad55.java
+++ Brad2Java/src/tw/brad/apps/Brad55.java
@@ -3,9 +3,14 @@ package tw.brad.apps;
 import java.awt.BorderLayout;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
@@ -26,18 +31,74 @@ public class Brad55 extends JFrame {
 	}
 	
 	private class MyPanel extends JPanel{
+		private Timer timer;
+		private int viewW, viewH, ballW, ballH;
+		private BufferedImage ballImg;
+		private LinkedList<BallTask> balls;
+		
+		MyPanel(){
+			try {
+				ballImg = ImageIO.read(new File("imgs/ball.png"));
+				ballW = ballImg.getWidth(); ballH = ballImg.getHeight();
+			} catch (IOException e) {
+				System.exit(0);
+			}
+
+			balls = new LinkedList<>();
+			
+			addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseClicked(MouseEvent e) {
+					createBall(e.getX(), e.getY());
+				}
+			});
+			
+			timer = new Timer();
+			timer.schedule(new RefreshView(), 0, 17);
+			
+		}
+		
+		private void createBall(int ex, int ey) {
+			
+		}
+		
+		
 		@Override
 		protected void paintComponent(Graphics g) {
 			super.paintComponent(g);
+			viewW = getWidth(); viewH = getHeight();
 			Graphics2D g2d = (Graphics2D)g;
 			
-			BufferedImage bimg;
-			try {
-				bimg = ImageIO.read(new File("imgs/ball.png"));
-				g2d.drawImage(bimg, 100, 100, null);
-			} catch (IOException e) {
+			
+		}
+		
+		private class RefreshView extends TimerTask {
+			@Override
+			public void run() {
+				repaint();
 			}
 		}
+		
+		private class BallTask extends TimerTask {
+			private int x, y, dx, dy;
+			BallTask(int x, int y){
+				this.dx = x; this.dy = y;
+				dx = dy = 4;
+			}
+			@Override
+			public void run() {
+				if (x < 0 || x + ballW > viewW) {
+					dx *= -1;
+				}
+				if (y < 0 || y + ballH > viewH) {
+					dy *= -1;
+				}
+				x += dx;
+				y += dy;
+			}
+		}
+
+		
 	}
 	
 
