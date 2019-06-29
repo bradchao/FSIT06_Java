8b12d7363b1e57308b9355b26aeecef5b7738f48
admin <admin@MBP.r2101.tciii.org.tw>
2019-06-29 10:30:29 +0800

    201906291030

---
+++ Brad2Java/src/tw/brad/apps/Racing.java
@@ -0,0 +1,69 @@
+package tw.brad.apps;
+
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Racing extends JFrame {
+	private JButton go;
+	private JLabel[] lanes = new JLabel[8];
+	private Car[] cars = new Car[8];
+	private int rank;
+	
+	public Racing() {
+		super("Racing");
+		setLayout(new GridLayout(9, 1));
+	
+		go = new JButton("Go!"); add(go);
+		for (int i=0; i<lanes.length; i++) {
+			lanes[i] = new JLabel((i+1) + ". ");
+			add(lanes[i]);
+		}
+		
+		go.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				go();
+			}
+		});
+		
+		setSize(800, 480);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+
+	private void go() {
+		for (int i=0; i<cars.length; i++) {
+			cars[i] = new Car(i);
+			cars[i].start();
+		}
+	}
+	
+	private class Car extends Thread {
+		private int lane;
+		Car(int lane){this.lane = lane;}
+		@Override
+		public void run() {
+			for (int i=0; i<100; i++) {
+				lanes[lane].setText(lanes[lane].getText() + ">");
+				if (i == 99) {
+					lanes[lane].setText(lanes[lane].getText() + ++rank);
+				}
+				try {
+					Thread.sleep(50 + (int)(Math.random()*200));
+				}catch(Exception e) {
+					
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		new Racing();
+	}
+
+}
